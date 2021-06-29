b83044749444fae439901cea365fc96c05680fca
sunilkumarmahajan <sunilkumarmahajan@gmail.com>
2021-06-29 17:06:02 +0200

    Goodboy-Oldboy.js

---
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -0,0 +1,12 @@
+var dogYearOfBirth = 2018;
+var dogYearFuture = 2025;
+var dogYear = dogYearFuture - dogYearOfBirth;
+var shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears = true )
+{
+    console.log("Your dog will be " + (dogYear*10) +  " dog years old in " + dogYearFuture);
+}
+ else
+{
+    console.log ("Your dog will be " + dogYear + " human years old in" + dogYearFuture); 
+}
