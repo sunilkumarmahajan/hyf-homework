6fed77d369ab241d902589c689970c1a36b5ac49
sunil mahajan <81170776+sunilkumarmahajan@users.noreply.github.com>
2021-07-04 16:20:35 +0200

    updates made as per the review

--- javascript/javascript1/week1/EzNamey.js
+++ javascript/javascript1/week1/EzNamey.js
@@ -1,5 +1,6 @@
-var firstWords = ["kia", "peugeot", "vw", "toyoto", "volvo", "benz", "nissan", "skoda", "hyundai", "ferrari"];
-var secondWords = ["picanto", "benzin", "gas", "i30", "polo", "gitz", "mini", "opel", "2008", "automat"];
-var randomNumber = Math.floor(Math.random() * 10);
-var startupName = firstWords[randomNumber]+ " " + secondWords[randomNumber];
-console.log("The startup: "+ startupName + " contains " + startupName.length + " characters");
+const firstWords = ["kia", "peugeot", "vw", "toyoto", "volvo", "benz", "nissan", "skoda", "hyundai", "ferrari"];
+const secondWords = ["picanto", "benzin", "gas", "i30", "polo", "gitz", "mini", "opel", "2008", "automat"];
+const randomNumber1 = Math.floor(Math.random() * firstWords.length);
+const randomNumber2 = Math.floor(Math.random() * secondWords.length);
+const startUpName = firstWords[randomNumber1]+ " " + secondWords[randomNumber2];
+console.log(`The startup ${startUpName} contains ${startUpName.length} characters `);
