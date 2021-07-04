964ff7d47b66bf272dfd800fc47be345911eb18d
sunil mahajan <81170776+sunilkumarmahajan@users.noreply.github.com>
2021-07-04 16:24:14 +0200

    updates  as per review

--- javascript/javascript1/week1/Goodboy-Oldboy.js
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -1,12 +1,7 @@
-var dogYearOfBirth = 2018;
-var dogYearFuture = 2025;
-var dogYear = dogYearFuture - dogYearOfBirth;
-var shouldShowResultInDogYears = true;
-if (shouldShowResultInDogYears = true )
-{
-    console.log("Your dog will be " + (dogYear*10) +  " dog years old in " + dogYearFuture);
-}
- else
-{
-    console.log ("Your dog will be " + dogYear + " human years old in" + dogYearFuture); 
-}
+const dogYearOfBirth = 2018;
+const dogYearFuture = 2025;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears) {    
+    console.log(`your dog will be ${dogYear*7} dog years old in ${dogYearFuture}`);} 
+else {    console.log(`your dog will be ${dogYear} human years old in ${dogYearFuture}`);}
