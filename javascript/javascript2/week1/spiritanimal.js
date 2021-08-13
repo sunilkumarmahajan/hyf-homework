05127ea121af1eb677fd8a6df9442ae1f01cb6fa
sunilkumarmahajan <sunilkumarmahajan@gmail.com>
2021-08-13 13:04:36 +0200

    spiritanimal.js file added

---
+++ javascript/javascript2/week1/spiritanimal.js
@@ -0,0 +1,18 @@
+const animalNames = ["Bear - It is deeply emotional", "Cat - symbolizes curiosity", 
+"Deer - balance between confidence and success", "Elephant - symbolizes wisdom",
+    "Horse - the guide of passion and drive", "Lion - represents heart and courage",
+    "Peacock-  represents resurrection and reinvention", 
+    "Tiger - represents raw feelings and emotions", "Butterfly - symbol of transformation"];
+const spiritAnimal = document.querySelector('.spiritAnimal');
+const button = document.querySelector(".submit");
+button.innerHTML = "submit";
+function generateSpiritName() {
+    const name = document.getElementsByClassName("name")[0].value;
+    if (name) {
+        spiritAnimal.innerHTML = `${name} : ${animalNames[Math.floor(Math.random() * 10)]}`;
+    }
+    else {
+        alert("please write your name")
+    }
+}
+button.addEventListener("click", generateSpiritName);
