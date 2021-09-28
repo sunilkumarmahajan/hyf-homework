7cf3ba270a4cb8c82d088aebc10e7a61c50a711c
sunil mahajan <81170776+sunilkumarmahajan@users.noreply.github.com>
2021-09-28 23:16:17 +0200

    Update class2hw.sql
    changed from inner join to join, even both gives the same results

--- databases/week2/class2hw.sql
+++ databases/week2/class2hw.sql
@@ -85,8 +85,8 @@ user_task.task_id = task.id;
 
 --using Join(Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
 SELECT * FROM user_task
-INNER JOIN task ON task_id=task.id
-INNER JOIN user ON user_id=user.id
+JOIN task ON task_id=task.id
+JOIN user ON user_id=user.id
 WHERE user.name='Maryrose Meadows' AND month(task.created) = 09;
 
 --Find how many tasks where created in each month, e.g. how many tasks were created in october, how many tasks were created in november, etc. 
